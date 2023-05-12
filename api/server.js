445534b28305e4641927dbbe2d7c68751046adc3
kunlefash <kunlefashmayop@gmail.com>
2023-05-12 10:51:13 +0100

    install jsonwebtoken as dependency

--- api/server.js
+++ api/server.js
@@ -1,28 +1,43 @@
-const express = require('express');
-const bodyParser = require('body-parser');const {
+const express = require("express");
+const bodyParser = require("body-parser");
+const {
   generateToken,
   verifyToken,
   hashPassword,
-  comparePasswords
-} = require('./src/auth.js');
-
+  comparePasswords,
+} = require("./src/auth.js");
+// const { MongoClient } = require("mongodb");
 
 const app = express();
 const PORT = 3000;
+// const uri = "mongodb://localhost:27017";
+// const client = new MongoClient(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+// // Connect to the database
+// client.connect((err) => {
+//   if (err) {
+//     console.error("Error connecting to MongoDB:", err);
+//     return;
+//   }
+//   console.log("Connected to MongoDB");
+
+// });
 
 app.use(bodyParser.json());
-
 let products = [];
 
 // GET all products
-app.get('/products', (req, res) => {
+app.get("/products", (req, res) => {
   res.json(products);
 });
 
 // GET a single product by ID
-app.get('/products/:id', (req, res) => {
+app.get("/products/:id", (req, res) => {
   const id = parseInt(req.params.id);
-  const product = products.find(p => p.id === id);
+  const product = products.find((p) => p.id === id);
 
   if (!product) {
     res.sendStatus(404);
@@ -33,7 +48,7 @@ app.get('/products/:id', (req, res) => {
 });
 
 // POST a new product
-app.post('/products', (req, res) => {
+app.post("/products", (req, res) => {
   const product = req.body;
   product.id = products.length + 1;
   products.push(product);
@@ -42,9 +57,9 @@ app.post('/products', (req, res) => {
 });
 
 // PUT (update) a product
-app.put('/products/:id', (req, res) => {
+app.put("/products/:id", (req, res) => {
   const id = parseInt(req.params.id);
-  const productIndex = products.findIndex(p => p.id === id);
+  const productIndex = products.findIndex((p) => p.id === id);
 
   if (productIndex === -1) {
     res.sendStatus(404);
@@ -57,9 +72,9 @@ app.put('/products/:id', (req, res) => {
 });
 
 // DELETE a product
-app.delete('/products/:id', (req, res) => {
+app.delete("/products/:id", (req, res) => {
   const id = parseInt(req.params.id);
-  const productIndex = products.findIndex(p => p.id === id);
+  const productIndex = products.findIndex((p) => p.id === id);
 
   if (productIndex === -1) {
     res.sendStatus(404);
